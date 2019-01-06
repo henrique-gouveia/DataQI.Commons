3166ac690deaeeab780d3e6d8ce9eccca9084b6e
Henrique Gouveia <henrique.gouveia17@gmail.com>
2019-01-06 17:55:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Net.Data.Commons
---
+++ src/Net.Data.Commons/Repository/ICrudRepository.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Net.Data.Commons.Repository
+{
+    public interface ICrudRepository<TEntity, TId> where TEntity : class, new()
+    {
+        IEnumerable<TEntity> FindAll();
+
+        Task<IEnumerable<TEntity>> FindAllAsync();
+
+        TEntity FindOne(TId id);
+
+        Task<TEntity> FindOneAsync(TId id);
+
+        bool Exists(TId id);
+
+        Task<bool> ExistsAsync(TId id);
+
+        void Insert(TEntity entity);
+
+        Task InsertAsync(TEntity entity);
+
+        void Save(TEntity entity);
+
+        Task SaveAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
