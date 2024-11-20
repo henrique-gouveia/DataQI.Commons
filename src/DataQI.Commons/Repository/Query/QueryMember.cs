2fcee21debafec38fb5e53090d84a2077c16597e
Henrique Gouveia <henrique.gouveia17@gmail.com>
2024-11-20 10:11:03 -0300

    feat(query): add support for new forms of existing keywords

--- src/DataQI.Commons/Repository/Query/QueryMember.cs
+++ src/DataQI.Commons/Repository/Query/QueryMember.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 using DataQI.Commons.Util;
@@ -9,13 +10,29 @@ namespace DataQI.Commons.Repository.Query
     {
         private static readonly string IsNotSmallLettersPattern = "(?!([a-z]))";
         private static readonly string LessOrGreaterGroupPattern = "(Less|Greater)+Than(Equal)?";
-        private static readonly string LikeGroupPattern = "Containing|Like|((End|Start)+ingWith)";
+        private static readonly string LikeGroupPattern = "Contain(s|ing)|Like|((End|Start)+(s|ing)With)";
         private static readonly string NotPattern = "Not";
         private static readonly string NotGroupPattern = $"({NotPattern})?(Null|Equal|Between|In|{LikeGroupPattern})";
-        private static readonly string TypePattern = $"((Is)?({NotGroupPattern})|{LessOrGreaterGroupPattern}){IsNotSmallLettersPattern}";
+        private static readonly string TypePattern = $"((Is)?({NotGroupPattern}|{LessOrGreaterGroupPattern})){IsNotSmallLettersPattern}";
 
         private static readonly Regex NotMatcher = new Regex($"{NotPattern}{IsNotSmallLettersPattern}");
         private static readonly Regex TypeMatcher = new Regex(TypePattern);
+
+        private static readonly IReadOnlyDictionary<MemberType, HashSet<string>> MemberTypeKeywordsMap = new Dictionary<MemberType, HashSet<string>>
+        {
+            { MemberType.Between, new HashSet<string>(new [] { "IsBetween", "Between" }) },
+            { MemberType.Containing, new HashSet<string>(new [] { "IsContaining", "Containing", "Contains" }) },
+            { MemberType.EndingWith, new HashSet<string>(new [] { "IsEndingWith", "EndingWith", "EndsWith" }) },
+            { MemberType.Equal, new HashSet<string>(new [] { "IsEqual", "Equal" }) },
+            { MemberType.GreaterThan, new HashSet<string>(new [] { "IsGreaterThan", "GreaterThan" }) },
+            { MemberType.GreaterThanEqual, new HashSet<string>(new []{ "IsGreaterThanEqual" , "GreaterThanEqual" }) },
+            { MemberType.In, new HashSet<string>(new [] { "IsIn", "In" }) },
+            { MemberType.LessThan, new HashSet<string>(new []{ "IsLessThan", "LessThan" }) },
+            { MemberType.LessThanEqual, new HashSet<string>(new []{ "IsLessThanEqual", "LessThanEqual" }) },
+            { MemberType.Like, new HashSet<string>(new [] { "IsLike", "Like" }) },
+            { MemberType.Null, new HashSet<string>(new [] { "IsNull", "Null" }) },
+            { MemberType.StartingWith, new HashSet<string>(new [] { "IsStartingWith", "StartingWith", "StartsWith" }) },
+        };
 
         private readonly Match typeMatch;
 
@@ -40,9 +57,11 @@ namespace DataQI.Commons.Repository.Query
 
         private MemberType TypeMatched()
         {
-            return typeMatch.Success && Enum.TryParse<MemberType>(typeMatch.Value, out var type)
-                ? type
-                : MemberType.SimpleProperty;
+            return MemberTypeKeywordsMap
+                .Where(kp => typeMatch.Success && kp.Value.Contains(typeMatch.Value))
+                .Select(kp => kp.Key)
+                .DefaultIfEmpty(MemberType.SimpleProperty)
+                .FirstOrDefault();
         }
 
         public bool HasNot { get; private set; }
